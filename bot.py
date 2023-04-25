49228691c50471461b3c9dd0be3cc616c0156ab8
chupikoff <54735023+chupikoff@users.noreply.github.com>
2023-04-25 22:58:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,36 +17,14 @@ def send_random_image(update, context):
     pic_filename = choice(random_photos_list)
     chat_id = update.effective_chat.id
     context.bot.send_photo(chat_id=chat_id, photo=open(pic_filename, 'rb'))
-
-def send_random_audio(update, context):
-    random_audio_list = glob('opt/audio/*.mp3')
-    mp3_filename = choice(random_audio_list)
-    chat_id = update.effective_chat.id
-    context.bot.send_audio(chat_id=chat_id, audio=open(mp3_filename, 'rb'))
-
-def send_random_diaz(update, context):
-    random_diaz_list = glob('opt/diaz/*.mp4')
-    diaz_filename = choice(random_diaz_list)
-    chat_id = update.effective_chat.id
-    context.bot.send_video(chat_id=chat_id, video=open(diaz_filename, 'rb'))
-    
-def send_random_tony(update, context):
-    random_tony_list = glob('opt/tony/*.mp4')
-    tony_filename = choice(random_tony_list)
-    chat_id = update.effective_chat.id
-    context.bot.send_video(chat_id=chat_id, video=open(tony_filename, 'rb'))            
-
-
+           
 def main():
 
     mybot = Updater(settings.API_KEY, use_context=True)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("image", send_random_image))
-    dp.add_handler(CommandHandler("audio", send_random_audio))
     dp.add_handler(CommandHandler("spb", send_random_spb))
-    dp.add_handler(CommandHandler("diaz", send_random_diaz))
-    dp.add_handler(CommandHandler("tony", send_random_tony))
     mybot.start_polling()
 
     mybot.idle()
